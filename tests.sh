2f97c61feb5509d54989a83e4abd66f1d230e5ae
braydonlowe <braydonslowe17@gmail.com>
2025-07-28 06:32:15 -0600

    Removed make file, changed tests.sh to run just the api test results.

--- tests.sh
+++ tests.sh
@@ -1,40 +1,7 @@
 #!/bin/bash
 
-set -e
-
-
-case "$1" in
-    #Go API tests
-    api)
-        echo ""
-        echo ""
-        echo "======= API Test Results ======="
-        go run api/mock_server.go
-	    go test ./_tests/api/api_test.go
-    ;;
-
-    #TS tests
-    ui)
-        echo ""
-        echo ""
-	    echo "======= UI Test Results ======="
-	    echo "UI tests not implemented"
-    ;;
-
-    #All tests
-    all|"")
-	    #test-api test-ui
-        #From here down we copy and paste the top two
-        echo ""
-        echo "======= API Test Results ======="
-        time go test ./_tests/api/api_test.go
-        echo ""
-        echo ""
-        echo "======= UI Test Results ======="
-        echo "UI tests not implemented"
-    ;;
-    *)
-        echo "Usage: ./test.sh [api|ui|all]"
-        exit 1
-    ;;
-esac
+echo ""
+echo ""
+echo "======= API Test Results ======="
+go run api/mock_server.go
+go test ./_tests/api/api_test.go
